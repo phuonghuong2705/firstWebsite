2be0d581f3eb738571576f8cffac7160b70e0742
huonghp <phuonghuong2705@gmail.com>
2025-01-15 16:24:55 +0700

    update package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
 import './bootstrap';
 import { createApp } from "vue";
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import Antd from 'ant-design-vue';
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(router)
+    .use(pinia)
     .use(Antd);
 app.mount("#app");
