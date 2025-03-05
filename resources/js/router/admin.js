919444942f6b1b501c142c50c9ceb104803fd6cd
huonghp <phuonghuong2705@gmail.com>
2025-03-05 18:13:43 +0700

    create base frontend for admin

--- resources/js/router/admin.js
+++ resources/js/router/admin.js
@@ -1,40 +1,22 @@
-// import Layout from "../layouts/layout.vue";
-// import Home from "../pages/Home/Home.vue";
+import Layout from "../layouts/layout.vue";
+import Home from "../pages/admin/home/Home.vue";
 // import Produce from "../pages/produce/index.vue";
 // import Cart from "../pages/cart/index.vue";
 const admin = [
     {   
-        // path: '/admin',
-        // component: Layout,
-        // children: [
-        //     {
-        //         title: 'Home',
-        //         path: 'home',
-        //         name: 'Home',
-        //         component: Home,
-        //         meta: {
-        //             title: 'Trang chủ',
-        //         },
-        //     },
-        //     {   
-        //         title: 'Produce',
-        //         path: 'produce',
-        //         component: Produce,
-        //         name: 'Produce',
-        //         meta: {
-        //             title: 'Sản phẩm',
-        //         },
-        //     },
-        //     {   
-        //         title: 'Cart',
-        //         path: 'cart',
-        //         component: Cart,
-        //         name: 'Cart',
-        //         meta: {
-        //             title: 'Giỏ hàng',
-        //         },
-        //     },
-        // ]
+        path: '/admin',
+        component: Layout,
+        children: [
+            {
+                title: 'Home',
+                path: 'home',
+                name: 'Home',
+                component: Home,
+                meta: {
+                    title: 'Trang chủ',
+                },
+            },
+        ]
     },
 ]
 
