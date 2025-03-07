1f76aa16c978b2f016df5129f64ee6e553da1b65
phuonghuong2705 <146051920+phuonghuong2705@users.noreply.github.com>
2025-03-07 14:40:42 +0700

    Revert "cài JWT qua composer"

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import client from "./client"
 import admin from "./admin"
-const routes = [...client, ...admin];
+const routes = [...client];
 
 const router = createRouter({
     history: createWebHistory(),
